d4b8fb72b68322766dc64e16c988a6fe9461d7f6
Krishnav <krishnavkns@gmail.com>
2026-02-23 18:09:20 +0530

    final ci cd test

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Name: Krishnav Shende | AppID: EXP6 | Updated|  WEBHOOK Working"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+print("=================================")
+print("Experiment 6 - CI/CD Working")
+print("Name: Krishnav Shende")
+print("AppID: EXP6")
+print("Webhook Trigger Successful")
+print("=================================")
